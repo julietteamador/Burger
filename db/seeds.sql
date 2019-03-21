b1f40041e2c124b82f073b2c7de1467b794ffed1
julietteamador <julietteamador@yahoo.com>
2019-03-21 18:15:44 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO `burgers_db`.`burgers` (`ID`, `burger_name`, `devoured`) VALUES ('1', 'cheese burger', '6');
+INSERT INTO `burgers_db`.`burgers` (`ID`, `burger_name`, `devoured`) VALUES ('2', 'veggie burger', '3');
+INSERT INTO `burgers_db`.`burgers` (`ID`, `burger_name`, `devoured`) VALUES ('3', 'turkey burger', '7');
+INSERT INTO `burgers_db`.`burgers` (`ID`, `burger_name`, `devoured`) VALUES ('4', 'plain burger', '2');
