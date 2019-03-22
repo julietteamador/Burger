ce31e52781aad56b5edb96d0707a34ff837ea473
julietteamador <julietteamador@yahoo.com>
2019-03-22 13:24:57 -0400

    handlebars

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
+var express = require("express");
 
-var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory to be served
-app.use(express.static("app/public"));
-
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
